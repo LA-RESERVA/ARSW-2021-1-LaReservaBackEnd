3906b948c64670fea74f5d572988fa0049876e1f
Federico Barrios <federico.barrios@mail.escuelaing.edu.co>
2021-04-12 18:25:22 -0500

    3

---
+++ src/main/java/co/edu/eci/LaReserva/entities/Horario.java
@@ -0,0 +1,88 @@
+package co.edu.eci.LaReserva.entities;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "horarios")
+public class Horario implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    @Column(name = "dia")
+    private String dia;
+    
+    @Column(name = "hora")
+    private LocalTime hora;
+    
+    @Column(name = "cancha")
+    private int cancha;
+    
+    @Column(name = "estado")
+    private boolean estado;
+
+    public Horario(int id, String dia, LocalTime hora, int cancha, boolean estado) {
+        this.id = id;
+        this.dia = dia;
+        this.hora = hora;
+        this.cancha = cancha;
+        this.estado = estado;
+    }
+
+    public Horario() {
+    
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public int getCancha() {
+        return cancha;
+    }
+
+    public void setCancha(int cancha) {
+        this.cancha = cancha;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Horario{" + "id=" + id + ", dia=" + dia + ", hora=" + hora + ", cancha=" + cancha + ", estado=" + estado + '}';
+    }
+}
---
+++ src/main/java/co/edu/eci/LaReserva/entities/Reserva.java
@@ -0,0 +1,63 @@
+package co.edu.eci.LaReserva.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "reservas")
+public class Reserva implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    @Column(name = "usuario")
+    private int usuario;
+    
+    @Column(name = "horario")
+    private int horario;
+
+    public Reserva(int id, int usuario, int horario) {
+        this.id = id;
+        this.usuario = usuario;
+        this.horario = horario;
+    }
+
+    public Reserva() {
+    
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getHorario() {
+        return horario;
+    }
+
+    public void setHorario(int horario) {
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" + "id=" + id + ", usuario=" + usuario + ", horario=" + horario + '}';
+    }
+}
