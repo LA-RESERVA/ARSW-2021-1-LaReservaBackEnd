de4a7b5e00db1c658de5609336b3a8a2098bb9c5
federico29 <federico.barrios@mail.escuelaing.edu.co>
2021-05-10 22:57:47 -0500

    Test

--- src/test/java/co/edu/eci/LaReserva/LaReservaApplicationTests.java
+++ src/test/java/co/edu/eci/LaReserva/LaReservaApplicationTests.java
@@ -1,13 +1,9 @@
 package co.edu.eci.LaReserva;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LaReservaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
